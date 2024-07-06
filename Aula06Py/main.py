abffad0250c040d2c0c13d0dec963489dbf28cad
unknown <fapessis@hotmail.com>
2024-07-06 13:57:41 -0300

    minhas alterações

--- Aula06Py/main.py
+++ Aula06Py/main.py
@@ -2,4 +2,4 @@ numero = int(input("Digite um número inteiro: "))
 fatorial = 1
 for i in range(1, numero + 1):
     fatorial *= i
-print(f"{numero} fatorial é: {fatorial}")
+print(f"{numero} fatorial é igual a: {fatorial}")
